1540f51dee8a85e6d4fec0d647193d462ed85a1d
mihaAM87 <MikhailAA1@yandex.ru>
2023-01-03 09:38:33 +0300

    1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
 
 function App() {
+  const [resultMessage, setResultMessage] = useState('Нет результата');
+  const [resultColor, setResultColor] = useState('text-warning');
+  const [arrLength, setArrLength] = useState('0');
+
+  function generate(max) {
+    let resultMessage = 'Неверный размер массива';
+    let resultColor = 'text-danger';
+    max = parseInt(max);
+    if (max > 0) {
+      const min = 1;
+
+      resultMessage = Math.floor(Math.random() * (max - min + 1)) + min;
+
+      resultColor = 'text-success';
+    }
+    setResultColor(resultColor);
+    setResultMessage(resultMessage);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="mainDiv">
+        <h1 className="text-info">Введите размер массива</h1>
+      </div>
+      <div className="mainDiv">
+        <input
+          type="number"
+          id="arrLength"
+          defaultValue={arrLength}
+          onChange={(e) => {
+            setArrLength(e.target.value);
+          }}
+          min="0"
+          className="text-primary"
+        />
+      </div>
+
+      <div className="mainDiv">
+        <Button
+          type="button"
+          id="randomBtn"
+          onClick={generate.bind(this, arrLength)}
+          className="btn btn-info text-white"
         >
-          Learn React
-        </a>
-      </header>
+          Сгенерировать
+        </Button>
+      </div>
+
+      <div className="mainDiv">
+        <h1 id="randomInt" className={resultColor}>
+          {resultMessage}
+        </h1>
+      </div>
     </div>
   );
 }
